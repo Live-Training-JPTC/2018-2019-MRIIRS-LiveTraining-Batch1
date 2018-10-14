1f29e0abd825580576ca5d115dad15389577a558
Nishant Choudhary <nishantchoudhary_mriirs@outlook.com>
2018-10-14 20:10:13 +0530

    first Prototype added

--- 4.Pike(A - Project 4)/4.Prototype/basic_es.py
+++ 4.Pike(A - Project 4)/4.Prototype/basic_es.py
@@ -1,10 +1,10 @@
 from elasticsearch import Elasticsearch
 def Create_connection():
-    es=None
-    es = Elasticsearch([{'host'='localhost',port=9200}]) 
+    es = None
+    es = Elasticsearch([{'host':'localhost', 'port':9200}]) 
     if es.ping():
         print "Connected"
     else:
         print "Could not establish a connection"
     return es
-
+Create_connection()
