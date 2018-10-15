56fe8a096b0cf2a2cfbb9f80d99da85a29209c35
Nishant Choudhary <nishantchoudhary_mriirs@outlook.com>
2018-10-15 23:14:40 +0530

    data extraction from ES

--- 4.Pike(A - Project 4)/4.Prototype/basic_es.py
+++ 4.Pike(A - Project 4)/4.Prototype/basic_es.py
@@ -1,10 +1,23 @@
 from elasticsearch import Elasticsearch
+import requests
+import json
+
 def Create_connection():
     es = None
     es = Elasticsearch([{'host':'localhost', 'port':9200}]) 
     if es.ping():
-        print "Connected"
+        print("Connected")
     else:
-        print "Could not establish a connection"
+        print("Could not establish a connection")
     return es
-Create_connection()
+def Extract_data(es_object):
+    res = es.search(index="company", doc_type="employees", body={"query": {"match_all": {}}})
+    print("%d documents found" % res['hits']['total'])
+    data={}
+    for doc in res['hits']['hits']:
+        dump=json.dumps(doc['_source'])
+        data[doc['_id']]=json.loads(dump)
+    print(data)
+es = Create_connection()
+Extract_data(es)
+
